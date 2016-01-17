c15041469dd937a52ca0320c99fddee7f2c4d7a2
Nathan McElwain <nathanmcelwain@ymail.com>
2016-01-17 00:47:02 -0800

    working version

--- js/app.js
+++ js/app.js
@@ -1,56 +1,216 @@
 $(document).ready(function(){
 
+	$('#comment-container').hide();
+	$('#show-comment-btn').hide();
+	$('#results-container').hide();
+	$('#track-player').hide();
+
+	var userPerma;
+	var personal;
+	var mePerma;
+	var mySongs = {};
+	var mySongIds = {};
+	var users = {};
+	var userSongs = {};
+	var userTrackIds = {};
+
+
+
 	SC.initialize({
 		client_id: '5c3759b4842b6a82f2a156a1b7aa0187',
-		redirect_uri: 'http://localhost:8080/cloudbrowse/callback.html'
-	});
-
-	
-
-	SC.connect().then(function() {
-		return SC.get('/me');
-	}).then(function(me) {
-		$('#ui').removeClass('hidden');
-		$('#ui h2 span').html(me.username+ '!');
-		$('#avatar').attr('src', me.avatar_url);
-		$('#city').html(me.city);
-		//SC.stream('/tracks/33427584').then(function(song) {
-			//song.play();
-			//console.log(song.title);
-			//});
+		redirect_uri: 'http://localhost.dev/cloudbrowse/callback.html'
 	});
 	
+	//auto connect
 
+	function login(){
+		SC.connect().then(function() {
+			return SC.get('/me');
+			}).then(function(me) {
+				personal = me.username;
+				mePerma = me.permalink_url;
+				$('#ui').removeClass('hidden');
+				$('#ui .right h2 span').html(me.username);
+				$('#avatar').attr('src', me.avatar_url);
+				$('#city').html(me.city);
+				$('#follows').html(me.followers_count);
+				$('#instructions').addClass('hidden');
+			});
+	}
 
-
-
-
-	function play(genre) {
-		console.log(genre)
-		SC.get('/tracks', {
-			genres: genre,
-			bpm: {
-				from:120
+	function me(){
+		$('#results-container').show();
+		$('#userResults').hide();
+		$('#results').show();
+		$('#track-player').show();
+		$('#results-container #returns span').html(personal);
+		SC.get('/me/tracks').then(function(tracks) {
+			for(var i =0; i<tracks.length; i++){
+				$('#results').append('<img src="'+tracks[i].artwork_url+'"/>'+'<br>'+'<li class="items">'+tracks[i].title+'</li>');
+				mySongs[tracks[i].title] = tracks[i].permalink_url;
+				mySongIds[tracks[i].title] = tracks[i].id;
 			}
-		}).then(function(tracks){
-			var random = Math.floor(Math.random()* 19);
-			SC.oEmbed(tracks[random].uri, {auto_play: true}, console.log(tracks.title), document.getElemenById('track-player'));
-
+		console.log(mySongIds);
 		});
 	}
 
-	//var link = $("#lookup").val();
-	$('#go').on('click', function(){
-		console.log(link);
-		play($("#lookup").val());
+	function searchUsers(query){
+		$('#results-container').show();
+		$('#userResults').html('')
+		$('#results').hide();
+		SC.get('/users', {
+			q: query
+		}).then(function(user){
+			for (var i = 0; i < user.length; i++) {
+				$('#userResults').append('<img src="' + user[i].avatar_url + '" />');
+				$('#userResults').append('<li class="items">'+ user[i].username + '</li>');
+				$('#results-container #returns span').html(query);
+				users[user[i].username] = user[i].id;
+			}
+		$('#userResults').show()
+		})
+
+	}
+
+	function getTracks(pick){
+		$('#userResults').html('');
+		SC.get('/users/'+pick+'/tracks', {
+			filter: 2
+		}).then(function(tracks){
+			for(var i = 0; i<tracks.length; i++){
+				$('#userResults').append('<img src="' + tracks[i].artwork_url + '" />'+'<br>'+'<li class="items">'+ tracks[i].title + '</li>');
+				$('#track-player #returns span').html(pick);
+				userSongs[tracks[i].title] = tracks[i].permalink_url;
+				userTrackIds[tracks[i].title] = tracks[i].id;
+			}
+		})
+
+	}
+
+	function player(tune){
+		$('#track-player').html('');
+		SC.oEmbed(tune, { auto_play: false, maxheight: 100}).then(function(oEmbed) {
+			$('#track-player').show();
+			$('#track-player').html(oEmbed.html);
+			//console.log('oEmbed response: ', oEmbed);
+		});
+
+	}
+
+	//play user tracks
+	$('#results-container #results').on('click', 'li', function(e){
+		var title = e.target.innerHTML;
+		if (mySongs.hasOwnProperty(title)){
+			player(mySongs[title])
+			$('#show-comment-btn').show();
+		}
+	});
+
+	$('#results-container #results').on('click', 'li', function(e){
+		$('#comment').html('');
+		var title = e.target.innerHTML;
+		console.log(title);
+		if (mySongIds.hasOwnProperty(title)){
+			var id = mySongIds.title;
+			SC.get('/tracks/'+id+'/comments').then(function(comments){
+				for(var i = 0; i<comments.length; i++){
+					$('#comment').append('<img src="'+comments[i].user.avatar_url+'" /><p class="cName">'+comments[i].user.username+'</p><p> on: '+comments[i].created_at+'</p><br>'+'<p class="track-comments">'+comments[i].body+'</p>');
+					$('#comment-container h1 span').html(title);
+				}
+			});
+		//$('#comment-container').show();
+		$('#show-comment-btn').show();
+		}
+	});
+
+	//get songs from search results
+	$('#results-container #userResults').on('click', 'li', function(e){
+		var username = e.target.innerHTML;
+		if (users.hasOwnProperty(username)){
+			getTracks(users[username]);
+		}
+	});
+
+	//play songs from results
+	$('#results-container #userResults').on('click', 'li', function(e){
+		var title = e.target.innerHTML;
+		if (userSongs.hasOwnProperty(title)) {
+			player(userSongs[title]);
+		}
+	});	
+
+	//show comments for current track
+	$('#results-container #userResults').on('click', 'li', function(e){
+		$('#comment').html('');
+		var title = e.target.innerHTML;
+		if (userTrackIds.hasOwnProperty(title)){
+			var id = userTrackIds[title];
+			SC.get('/tracks/'+id+'/comments').then(function(comments){
+				for(var i = 0; i<comments.length; i++){
+					$('#comment').append('<img src="'+comments[i].user.avatar_url+'" /><p class="cName">'+comments[i].user.username+'</p><p> on: '+comments[i].created_at+'</p><br>'+'<p class="track-comments">'+comments[i].body+'</p>');
+					$('#comment-container h1 span').html(title);
+				}
+			});
+		$('#show-comment-btn').show();
+		}
+	});
+
+	$('#show-comment-btn').click(function(){
+				$('#comment-container').toggle();
+
+			});
+	
+	document.querySelector('#get-my-tracks').addEventListener('click', function(){
+		$('#userResults').html('');
+		$('#track-player').html('');
+		$('#results').html('');
+		$('#comment').html('');
+		$('#comment-container').hide();
+		$('#show-comment-btn').hide();
+		me();
 	});
 
 
+	document.querySelector('#log').addEventListener('click', function(){
+		login();
+		console.log('click');
+	});
 
+	$('#forms').on('submit', function(){
+		var searchVal = $('#lookup').val();
+		$('#track-player').html('');
+		$('#userResults').html('');
+		$('#results').html('');
+		$('#resulter span').html('');
+		$('#returns span').html('');
+		$('#comment-container').hide();
+		$('#show-comment-btn').hide();
+		$('#lookup').val('');
+		searchUsers(searchVal);
+	});
 
+	function init(){
+		$('#lookup').val('');
+		$('#userResults').html('');
+		$('#results').html('');
+		$('#resulter span').html('');
+		$('#returns span').html('');
+	}
 
-	
-	
+	//function play(genre) {
+		//console.log(genre)
+		//SC.get('/tracks', {
+			//genres: genre,
+			//bpm: {
+				//from:120
+			//}
+		//}).then(function(tracks){
+			//var random = Math.floor(Math.random()* 19);
+			//SC.oEmbed(tracks[random].uri, {auto_play: true}, console.log(genre), document.getElemenById('track-player'));
+
+		//});
+	//}
+
 
 
 
